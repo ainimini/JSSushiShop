387aa1778614770a27937c6b98685c51e00da800
ainimini <houxinjiemail@163.com>
2020-03-31 18:09:53 +0800

    [fix]: Fixed an error in the PayFegin call to the order service

--- JSSushiShop-Service/js-framework-parent/js-service/js-service-pay/src/main/java/com/junshou/service/pay/config/security/ResourceServerConfig.java
+++ JSSushiShop-Service/js-framework-parent/js-service/js-service-pay/src/main/java/com/junshou/service/pay/config/security/ResourceServerConfig.java
@@ -81,6 +81,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         //所有请求必须认证通过
         http.authorizeRequests()
+                .antMatchers("/wxpay/query").permitAll()
                 .antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui",
                         "/swagger-resources", "/swagger-resources/configuration/security",
                         "/swagger-ui.html", "/webjars/**").permitAll()
